2cba8166dce6ce73f02087a96dd5e2b163fe99ba
Mec <mechanism.minecraft.mec@gmail.com>
2022-09-20 12:31:31 +0800

    D304 AC

--- ICT2 Course/C++/HKOI/DSE/D304.cpp
+++ ICT2 Course/C++/HKOI/DSE/D304.cpp
@@ -3,13 +3,12 @@ using namespace std;
 int main() {
     string n;
     cin >> n;
-    char b[n.size()];
+    string b;
     for (int i = 0; i < n.size(); i++) {
-        b[i] = '_';
+        b = b + '_';
     }
-
+    char a;
     while (b != n) {
-        char a;
         cin >> a;
         for (int i = 0; i < n.size(); i++) {
             if (n[i] == a) {
