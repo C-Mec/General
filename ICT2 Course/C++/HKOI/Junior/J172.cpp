339d3e33f282984dfc2eee696a7db581999d9bf7
MecHome <mechanism.minecraft.mec@gmail.com>
2022-09-28 17:54:43 +0800

    J172.cpp AC

--- ICT2 Course/C++/HKOI/Junior/J172.cpp
+++ ICT2 Course/C++/HKOI/Junior/J172.cpp
@@ -3,18 +3,23 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    if (k <= (float)n / 2) {
+    if (k < (float)sqrt(n)) {
         cout << "Impossible" << endl;
         return 0;
     } else {
-        for (int i = 1; i < k; i++) {
-            cout << i << " ";
+        int m = n / k;
+        int t = 0;
+        for (int i = 0; i < k; i++) {
+            if (n % k != 0 && i >= n % k) {
+                for (int j = m + t + 1; j > t; j--) {
+                    cout << j + i * m << " ";
+                }
+                t++;
+            } else {
+                for (int j = m; j > 0; j--) {
+                    cout << j + i * m << " ";
+                }
+            }
         }
-        cout << n << " ";
-        for (int i = n - 1; i > k; i--) {
-            cout << i << " ";
-        }
-        if (n != k)
-            cout << k << endl;
     }
 }
