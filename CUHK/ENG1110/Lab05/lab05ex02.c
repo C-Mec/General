430fa414536f3290c9da5083c785fa70075d4e81
MecHome <mechanism.minecraft.mec@gmail.com>
2025-10-02 17:17:40 +0800

    Lab05 prelab

---
+++ CUHK/ENG1110/Lab05/lab05ex02.c
@@ -0,0 +1,33 @@
+/**
+ * ENGG1110 Problem Solving by Programming
+ *
+ * I declare that the assignment here submitted is original
+ * except for source material explicitly acknowledged,
+ * and that the same or closely related material has not been
+ * previously submitted for another course.
+ * I also acknowledge that I am aware of University policy and
+ * regulations on honesty in academic work, and of the disciplinary
+ * guidelines and procedures applicable to breaches of such
+ * policy and regulations, as contained in the website.
+ *
+ * University Guideline on Academic Honesty:
+ *http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ * Student Name : Yung Sze Wai
+ * Student ID   : 1155259203
+ * Class/Section: ENGG1110A
+ * Date         : 021025
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    printf("Guess my age? ");
+    scanf("%d", &n);
+    while (n != 7) {
+        printf("Guess my age? ");
+        scanf("%d", &n);
+    }
+    printf("You got it!");
+}
